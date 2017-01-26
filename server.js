3d482d3390682fb7598e778f736d57d015939251
Connor McElroy <cmcelroy@ksu.edu>
2017-01-26 13:30:12 -0800

    sdfklj

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
+"use strict";
+
 /**
  * server.js
  * This file defines the server for a
  * simple photo gallery web app.
  */
- 
+var http = require('http');
+var port = 3000;
+
+var server = http.createServer((req, res) => {
+  res.end("OK");
+});
+
+server.listen(port, ()=>{
+  console.log("Listening on Port " + port);
+});
